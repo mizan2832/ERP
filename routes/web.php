b855921233d082bef15683b4e4bca121490d1319
mizan2832 <mrahman5037@gmail.com>
2023-07-28 22:04:42 +0600

    permission

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BuyerController;
 
-
-
-Route::get('/', function () {
+Auth::routes();
+Route::get('/',function(){
     return view('pages.home');
-});
+})->middleware(['auth','role:admin'])->name('admin.index');
+
 
 Route::get('/library', function () {
     return view('pages.library');
 });
 
- 
+
 Route::get('/buyer', [BuyerController::class, 'index'])->name('buyer');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
